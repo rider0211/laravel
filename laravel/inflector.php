228772adbc7156ab4f2610497271bef18889343d
rider0211 <codemaster9428@gmail.com>
2011-11-09 21:55:21 -0600

    moved session class.

--- laravel/inflector.php
+++ laravel/inflector.php
@@ -102,15 +102,13 @@ class Inflector {
 	 * @var array
 	 */
 	private static $uncountable = array(
-		'audio',
 		'equipment',
+		'data',
 		'deer',
 		'fish',
-		'gold',
 		'information',
 		'money',
 		'rice',
-		'police',
 		'series',
 		'sheep',
 		'species',
@@ -140,7 +138,7 @@ class Inflector {
 	 */
 	public static function plural($value, $count = null)
 	{
-		if ( ! is_null($count) and $count == 1) return $value;
+		if ( ! is_null($count) and $count <= 1) return $value;
 
 		$irregular = array_flip(static::$irregular);
 
