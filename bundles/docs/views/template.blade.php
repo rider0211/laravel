9c8e0998e9dbc0b5d9c64719c6c9df6b89b64101
rider0211 <codemaster9428@gmail.com>
2012-04-02 11:30:53 -0500

    Adding Schema::rename('oldtable','newtable') support

--- bundles/docs/views/template.blade.php
+++ bundles/docs/views/template.blade.php
@@ -22,7 +22,7 @@
 		</header>
 		<div role="main" class="main">
 			<aside class="sidebar">
-				{{ $sidebar }}
+				@include('docs::menu')
 			</aside>
 			<div class="content">
 				@yield('content')
