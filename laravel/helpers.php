acef288633a50192c88ee86bc4e1e55c26d6d285
rider0211 <codemaster9428@gmail.com>
2012-07-08 08:42:13 +0900

    Add Japanese language resource files.

--- laravel/helpers.php
+++ laravel/helpers.php
@@ -581,15 +581,3 @@ function get_cli_option($option, $default = null)
 
 	return value($default);
 }
-	
-/**
- * Calculate the human-readable file size (with proper units).
- *
- * @param  int     $size
- * @return string
- */
-function get_file_size($size)
-{
-	$units = array('Bytes', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB');
-	return @round($size / pow(1024, ($i = floor(log($size, 1024)))), 2).' '.$units[$i];
-}
