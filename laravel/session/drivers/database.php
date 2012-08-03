6dba01b6c6c63eef0b60961d60d343323316b64a
rider0211 <codemaster9428@gmail.com>
2012-08-03 18:17:48 +0300

    phpdoc fixes

--- laravel/session/drivers/database.php
+++ laravel/session/drivers/database.php
@@ -10,7 +10,7 @@ class Database extends Driver implements Sweeper {
 	 *
 	 * @var Connection
 	 */
-	protected $connection;
+	private $connection;
 
 	/**
 	 * Create a new database session driver.
