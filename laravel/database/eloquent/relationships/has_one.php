c5f27bcf9ddd1d6010faf89cae1103b03215699a
rider0211 <codemaster9428@gmail.com>
2012-10-11 08:14:55 +0200

    changed date format in validation documentation to match ISO8601

--- laravel/database/eloquent/relationships/has_one.php
+++ laravel/database/eloquent/relationships/has_one.php
@@ -38,21 +38,19 @@ class Has_One extends Has_One_Or_Many {
 	{
 		$foreign = $this->foreign_key();
 
-		$children_hash = array();
+		$dictionary = array();
+
 		foreach ($children as $child)
 		{
-			if (array_key_exists($child->pivot->$foreign, $children_hash))
-			{
-				continue;
-			}
-
-			$children_hash[$child->pivot->$foreign] = $child;
+			$dictionary[$child->$foreign] = $child;
 		}
 
 		foreach ($parents as $parent)
 		{
-			if (array_key_exists($parent->get_key(), $children_hash))
-				$parent->relationships[$relationship] = $children_hash[$parent->get_key()];
+			if (array_key_exists($key = $parent->get_key(), $dictionary))
+			{
+				$parent->relationships[$relationship] = $dictionary[$key];
+			}
 		}
 	}
 
