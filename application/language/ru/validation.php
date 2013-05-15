c1380ce605e0146f1c82852cd6b70ea7053662a5
rider0211 <codemaster9428@gmail.com>
2013-05-15 19:05:04 +0200

    Fixes variable export for Windows.

--- application/language/ru/validation.php
+++ application/language/ru/validation.php
@@ -38,7 +38,7 @@ return array(
 	"countmin"       => "The :attribute must have at least :min selected elements.",
 	"different"      => "Поля :attribute и :other должны различаться.",
 	"email"          => "Поле :attribute имеет неверный формат.",
-	"exists"         => "Выбранное значение для :attribute не верно.",
+	"exists"         => "Выбранное значение для :attribute уже существует.",
 	"image"          => "Поле :attribute должно быть картинкой.",
 	"in"             => "Выбранное значение для :attribute не верно.",
 	"integer"        => "Поле :attribute должно быть целым числом.",
