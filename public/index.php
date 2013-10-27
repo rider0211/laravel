9f46892f62681bb28c693af8db53a777f0e52b26
rider0211 <codemaster9428@gmail.com>
2013-10-26 19:03:30 -0500

    Tweak session config.

--- public/index.php
+++ public/index.php
@@ -42,21 +42,10 @@ $app = require_once __DIR__.'/../bootstrap/start.php';
 | Once we have the application, we can simply call the run method,
 | which will execute the request and send the response back to
 | the client's browser allowing them to enjoy the creative
-| and wonderful applications we have created for them.
+| and wonderful application we have whipped up for them.
 |
 */
 
-$app->run();
+use Symfony\Component\HttpFoundation\Request;
 
-/*
-|--------------------------------------------------------------------------
-| Shutdown The Application
-|--------------------------------------------------------------------------
-|
-| Once the app has finished running, we will fire off the shutdown events
-| so that any final work may be done by the application before we shut
-| down the process. This is the last thing to happen to the request.
-|
-*/
-
-$app->shutdown();
+$app->run(Request::createFromGlobals());
