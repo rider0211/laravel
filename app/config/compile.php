d375404b18b8cd6f071c7a68dcdc4133bd0be710
rider0211 <codemaster9428@gmail.com>
2014-08-06 11:30:27 -0700

    Missing validation sentence for timezone.

--- app/config/compile.php
+++ app/config/compile.php
@@ -13,23 +13,6 @@ return array(
 	|
 	*/
 
-	'files' => array(
-		//
-	),
 
-	/*
-	|--------------------------------------------------------------------------
-	| Compiled File Providers
-	|--------------------------------------------------------------------------
-	|
-	| Here you may list service providers which define a "compiles" function
-	| that returns additional files that should be compiled, providing an
-	| easy way to get common files from any packages you are utilizing.
-	|
-	*/
-
-	'providers' => array(
-		//
-	),
 
 );
