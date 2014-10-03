df831af2dcb627f53f744890040cc088eeef886c
rider0211 <codemaster9428@gmail.com>
2014-10-02 19:36:03 -0500

    Tweak some paths.

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -1,5 +1,6 @@
 <?php namespace App\Providers;
 
+use Illuminate\Contracts\Events\Dispatcher;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 
 class EventServiceProvider extends ServiceProvider {
