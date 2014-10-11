138de81eaa693737a0b94c1a55d4733373409a51
rider0211 <codemaster9428@gmail.com>
2014-10-11 23:23:02 +0300

    Add missing gulp-utils package to package.json

--- app/Http/Middleware/CsrfMiddleware.php
+++ app/Http/Middleware/CsrfMiddleware.php
@@ -12,17 +12,26 @@ class CsrfMiddleware implements Middleware {
 	 * @param  \Illuminate\Http\Request  $request
 	 * @param  \Closure  $next
 	 * @return mixed
-	 * 
-	 * @throws TokenMismatchException
 	 */
 	public function handle($request, Closure $next)
 	{
-		if ($request->session()->token() != $request->input('_token'))
+		if ($request->method() == 'GET' || $this->tokensMatch($request))
 		{
-			throw new TokenMismatchException;
+			return $next($request);
 		}
 
-		return $next($request);
+		throw new TokenMismatchException;
+	}
+
+	/**
+	 * Determine if the session and input CSRF tokens match.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @return bool
+	 */
+	protected function tokensMatch($request)
+	{
+		return $request->session()->token() == $request->input('_token');
 	}
 
 }
