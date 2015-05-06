b48df1f9c57418d2dbf27b695a2fb9e2b018109c
rider0211 <codemaster9428@gmail.com>
2015-05-06 18:44:25 -0400

    change version from ~1.0.0 to ^1.0.0

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -3,14 +3,18 @@
 use Closure;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as BaseVerifier;
 
-class VerifyCsrfToken extends BaseVerifier
-{
-    /**
-     * The URIs that should be excluded from CSRF verification.
-     *
-     * @var array
-     */
-    protected $except = [
-        //
-    ];
+class VerifyCsrfToken extends BaseVerifier {
+
+	/**
+	 * Handle an incoming request.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  \Closure  $next
+	 * @return mixed
+	 */
+	public function handle($request, Closure $next)
+	{
+		return parent::handle($request, $next);
+	}
+
 }
