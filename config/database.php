62bbedfc40d2c2c831a612a9d94a561acf78ac3f
rider0211 <codemaster9428@gmail.com>
2015-11-19 23:01:29 +0000

    Change redirect when authenticated

--- config/database.php
+++ config/database.php
@@ -116,9 +116,9 @@ return [
         'cluster' => false,
 
         'default' => [
-            'host'     => env('REDIS_HOST', '127.0.0.1'),
-            'port'     => env('REDIS_PORT', 6379),
-            'database' => env('REDIS_DB', 0),
+            'host'     => '127.0.0.1',
+            'port'     => 6379,
+            'database' => 0,
         ],
 
     ],
