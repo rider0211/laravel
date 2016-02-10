7bd49830e92e77f083d9069622d836a60f5d53d4
rider0211 <codemaster9428@gmail.com>
2016-02-10 16:55:42 -0600

    adding public directory to app storage

--- config/database.php
+++ config/database.php
@@ -62,7 +62,6 @@ return [
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
             'strict'    => false,
-            'engine'    => null,
         ],
 
         'pgsql' => [
