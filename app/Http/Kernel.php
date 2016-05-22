3fa5e2c0e443e7d6150f77e5a2260c8fc3ce27b7
rider0211 <codemaster9428@gmail.com>
2016-05-22 14:50:39 -0400

    Make the Authenticate middleware throw an AuthenticationException

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -29,12 +29,10 @@ class Kernel extends HttpKernel
             \Illuminate\Session\Middleware\StartSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
-            \Illuminate\Routing\Middleware\SubstitutesBindings::class,
         ],
 
         'api' => [
             'throttle:60,1',
-            'bindings',
         ],
     ];
 
@@ -48,7 +46,6 @@ class Kernel extends HttpKernel
     protected $routeMiddleware = [
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
-        'bindings' => \Illuminate\Routing\Middleware\SubstitutesBindings::class,
         'can' => \Illuminate\Foundation\Http\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
