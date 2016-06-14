0dcf4ed1f835ca042120c3f25040e43cbef38770
rider0211 <codemaster9428@gmail.com>
2016-06-14 20:15:26 +0200

    List supported drives in cache and broadcasting configs

--- config/database.php
+++ config/database.php
@@ -62,7 +62,7 @@ return [
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
-            'strict' => true,
+            'strict' => false,
             'engine' => null,
         ],
 
@@ -76,7 +76,6 @@ return [
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
-            'sslmode' => 'prefer',
         ],
 
     ],
