b96318fb9df355c27c17e005c2bf25e75d94c965
rider0211 <codemaster9428@gmail.com>
2016-08-04 11:55:08 +0200

    give newbees a package service providers section, preventing adding after application

--- config/queue.php
+++ config/queue.php
@@ -38,14 +38,14 @@ return [
             'driver' => 'database',
             'table' => 'jobs',
             'queue' => 'default',
-            'retry_after' => 90,
+            'expire' => 90,
         ],
 
         'beanstalkd' => [
             'driver' => 'beanstalkd',
             'host' => 'localhost',
             'queue' => 'default',
-            'retry_after' => 90,
+            'ttr' => 90,
         ],
 
         'sqs' => [
@@ -61,7 +61,7 @@ return [
             'driver' => 'redis',
             'connection' => 'default',
             'queue' => 'default',
-            'retry_after' => 90,
+            'expire' => 90,
         ],
 
     ],
