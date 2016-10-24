230d7969cb033ffe3a2a4db42eecb76f4c70d8c6
rider0211 <codemaster9428@gmail.com>
2016-10-24 16:07:22 -0500

    use utf8mb4 as default character set

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 const elixir = require('laravel-elixir');
 
-require('laravel-elixir-vue-2');
+require('laravel-elixir-vue');
 
 /*
  |--------------------------------------------------------------------------
@@ -9,11 +9,11 @@ require('laravel-elixir-vue-2');
  |
  | Elixir provides a clean, fluent API for defining some basic Gulp tasks
  | for your Laravel application. By default, we are compiling the Sass
- | file for your application as well as publishing vendor resources.
+ | file for our application, as well as publishing vendor resources.
  |
  */
 
-elixir((mix) => {
+elixir(mix => {
     mix.sass('app.scss')
        .webpack('app.js');
 });
