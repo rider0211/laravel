7126a1b092f685b686adac6bb5a70aa866c10a9c
rider0211 <codemaster9428@gmail.com>
2016-12-29 13:17:27 +0000

    Remove the mail driver as recommended, and add array

--- config/app.php
+++ config/app.php
@@ -166,7 +166,8 @@ return [
         /*
          * Package Service Providers...
          */
-        Laravel\Tinker\TinkerServiceProvider::class,
+
+        //
 
         /*
          * Application Service Providers...
