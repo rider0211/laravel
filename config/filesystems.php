436e81f4a1ed2c37f9ea64f0b61b8f31960829a3
rider0211 <codemaster9428@gmail.com>
2017-11-09 20:46:09 +0100

    Support custom URLs for AWS storage

--- config/filesystems.php
+++ config/filesystems.php
@@ -57,9 +57,10 @@ return [
 
         's3' => [
             'driver' => 's3',
-            'key' => env('AWS_ACCESS_KEY_ID'),
-            'secret' => env('AWS_SECRET_ACCESS_KEY'),
-            'region' => env('AWS_DEFAULT_REGION'),
+            'url' => env('AWS_URL'),
+            'key' => env('AWS_KEY'),
+            'secret' => env('AWS_SECRET'),
+            'region' => env('AWS_REGION'),
             'bucket' => env('AWS_BUCKET'),
         ],
 
