af1821ef676bf854180cbe87ab6c41105df544b4
rider0211 <codemaster9428@gmail.com>
2017-12-18 10:40:39 -0600

    prefer stable

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -57,6 +57,5 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        'cache' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
     ];
 }
