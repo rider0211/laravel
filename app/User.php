f145e4db203aae17d3d54a1ed37bd8a5c02bd3d1
rider0211 <codemaster9428@gmail.com>
2018-09-06 14:02:17 +0200

    Add missing Mailgun 'endpoint' option

--- app/User.php
+++ app/User.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use Illuminate\Auth\MustVerifyEmail;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Contracts\Auth\MustVerifyEmail as MustVerifyEmailContract;
