dd09f1318af9e42db4eed8a58961868142e99b6f
rider0211 <codemaster9428@gmail.com>
2018-10-16 22:36:12 +0200

    changed syntax for validation

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -17,7 +17,8 @@ $factory->define(App\User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'email_verified_at' => now(),
+        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
         'remember_token' => str_random(10),
     ];
 });
