76bc2d66b51e98e0558e94c3cd946dfb6b555171
rider0211 <codemaster9428@gmail.com>
2019-02-04 08:08:58 -0500

    Add sponsor.

--- app/User.php
+++ app/User.php
@@ -27,13 +27,4 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
-
-    /**
-     * The attributes that should be mutated to dates.
-     *
-     * @var array
-     */
-    protected $dates = [
-        'email_verified_at',
-    ];
 }
