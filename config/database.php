897ce90eec573ed5c1c6d6227ead2cbd127c2863
rider0211 <codemaster9428@gmail.com>
2019-05-06 08:30:33 -0500

    update version

--- config/database.php
+++ config/database.php
@@ -1,7 +1,5 @@
 <?php
 
-use Illuminate\Support\Str;
-
 return [
 
     /*
@@ -119,7 +117,6 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'predis'),
-            'prefix' => Str::slug(env('APP_NAME', 'laravel'), '_').'_database_',
         ],
 
         'default' => [
