972d6b194bf80a98b04c17de2c331ae1fe7ad58a
rider0211 <codemaster9428@gmail.com>
2019-12-25 18:05:29 +0300

    Fix types consistency in database config (#5191)

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,5 +1,6 @@
 <?php
 
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
 use App\User;
 use Faker\Generator as Faker;
 use Illuminate\Support\Str;
@@ -14,8 +15,6 @@ use Illuminate\Support\Str;
 | model instances for testing / seeding your application's database.
 |
 */
-
-/* @var \Illuminate\Database\Eloquent\Factory $factory */
 
 $factory->define(User::class, function (Faker $faker) {
     return [
